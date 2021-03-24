aaf8c792857e4e17667a0c0be5da9ab7333b2cf9
BobbySinclusto <allencbenjamin@gmail.com>
2021-03-24 11:46:35 -0700

    added mobile controls

--- public/js/game.js
+++ public/js/game.js
@@ -2,6 +2,8 @@ const WIDTH = 1920;
 const HEIGHT = 1080;
 const MAX_TANK_SPEED = 100;
 const MAX_BULLET_SPEED = 110;
+let is_mobile = false;
+let should_shoot = false;
 
 var config = {
     type: Phaser.AUTO,
@@ -24,9 +26,22 @@ var config = {
 
 var game = new Phaser.Game(config);
 
+function checkMob() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+}
+
 function preload() {
     this.load.image('ship', 'assets/playerShip1_blue.png');
     this.load.image('otherPlayer', 'assets/enemyBlack5.png')
+    if (checkMob()) {
+        is_mobile = true;
+        // Add joystick module
+        this.load.plugin('rexvirtualjoystickplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexvirtualjoystickplugin.min.js', true);
+        // Add button module
+        this.load.plugin('rexbuttonplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexbuttonplugin.min.js', true);
+    }
 }
 
 function create() {
@@ -39,6 +54,34 @@ function create() {
     this.walls = this.physics.add.group();
     this.myBullets = this.physics.add.group();
     this.otherBullets = this.physics.add.group();
+
+    // Check if on mobile
+    if (is_mobile) {
+        // Add joystick
+        let margin = HEIGHT / 16;
+        let radius = WIDTH / 16;
+        this.joyStick = this.plugins.get('rexvirtualjoystickplugin').add(this, {
+            x: margin + radius,
+            y: HEIGHT - margin - radius,
+            radius: radius,
+            base: this.add.circle(0, 0, radius, 0x888888),
+            thumb: this.add.circle(0, 0, radius / 2, 0xcccccc),
+            // dir: '8dir',   // 'up&down'|0|'left&right'|1|'4dir'|2|'8dir'|3
+            forceMin: 70,
+            // enable: true
+        });
+        this.cursors = this.joyStick.createCursorKeys();
+
+        // Add fire button
+        let button = this.add.circle(WIDTH - margin - radius, HEIGHT - margin - radius, radius, 0x880000);
+        this.fireButton = this.plugins.get('rexbuttonplugin').add(button);
+        this.fireButton.on('click', function () {
+            should_shoot = true;
+        });
+    }
+    else {
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
 
     this.socket.on('currentPlayers', function (players) {
         Object.keys(players).forEach(function (id) {
@@ -78,7 +121,6 @@ function create() {
     this.socket.on('shotBullet', function(bulletData) {
         shootBullet(self, bulletData.x, bulletData.y, bulletData.rot, false);
     });
-    this.cursors = this.input.keyboard.createCursorKeys();
 
     // Players and myBullets collide with walls
     this.physics.add.collider(this.walls, this.playerGroup);
@@ -135,16 +177,25 @@ function update() {
             this.ship.setVelocityY(0);
         }
 
-        if (this.cursors.space.isDown) {
-            if (this.isShooting == false) {
-                this.isShooting = true;
-                // Shoot a bullet
-                shootBullet(this, this.ship.x, this.ship.y, this.ship.rotation, true);
-                this.socket.emit('shotBullet', {x: this.ship.x, y: this.ship.y, rot: this.ship.rotation});
+        if (!is_mobile) {
+            if (this.cursors.space.isDown) {
+                if (this.isShooting == false) {
+                    this.isShooting = true;
+                    // Shoot a bullet
+                    shootBullet(this, this.ship.x, this.ship.y, this.ship.rotation, true);
+                    this.socket.emit('shotBullet', {x: this.ship.x, y: this.ship.y, rot: this.ship.rotation});
+                }
+            }
+            else {
+                this.isShooting = false;
             }
         }
         else {
-            this.isShooting = false;
+            if (should_shoot) {
+                should_shoot = false;
+                shootBullet(this, this.ship.x, this.ship.y, this.ship.rotation, true);
+                this.socket.emit('shotBullet', {x: this.ship.x, y: this.ship.y, rot: this.ship.rotation});
+            }
         }
 
         this.physics.world.wrap(this.ship, 5);
