f9ce30bb436cf2edceaea9820a68a6f5d6853874
BobbySinclusto <allencbenjamin@gmail.com>
2021-03-24 00:18:07 -0700

    implemented maze generator

--- public/js/game.js
+++ public/js/game.js
@@ -1,8 +1,13 @@
+const WIDTH = 1920;
+const HEIGHT = 1080;
+const MAX_TANK_SPEED = 100;
+const MAX_BULLET_SPEED = 110;
+
 var config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
-    width: 800,
-    height: 600,
+    width: WIDTH,
+	height: HEIGHT,
     physics: {
         default: 'arcade',
         arcade: {
@@ -28,6 +33,13 @@ function create() {
     var self = this;
     this.socket = io();
     this.otherPlayers = this.physics.add.group();
+    this.playerGroup = this.physics.add.group();
+    this.initialX = -1;
+    this.initialY = -1;
+    this.walls = this.physics.add.group();
+    this.myBullets = this.physics.add.group();
+    this.otherBullets = this.physics.add.group();
+
     this.socket.on('currentPlayers', function (players) {
         Object.keys(players).forEach(function (id) {
             if (players[id].playerId === self.socket.id) {
@@ -56,12 +68,38 @@ function create() {
             }
         });
     });
+    this.socket.on('current_map', function(current_map) {
+        console.log(current_map);
+        draw_map(self, current_map);
+        // Randomly select initial player position based on current map
+        self.initialX = Math.floor(Math.random() * self.map_width / self.grid_size) * self.grid_size + self.grid_size * 0.5 + WIDTH / 2 - self.map_width / 2;
+        self.initialY = Math.floor(Math.random() * self.map_height / self.grid_size) * self.grid_size + self.grid_size * 0.5 + HEIGHT / 2 - self.map_height / 2;
+    });
+    this.socket.on('shotBullet', function(bulletData) {
+        shootBullet(self, bulletData.x, bulletData.y, bulletData.rot, false);
+    });
     this.cursors = this.input.keyboard.createCursorKeys();
+
+    // Players and myBullets collide with walls
+    this.physics.add.collider(this.walls, this.playerGroup);
+    this.physics.add.collider(this.walls, this.myBullets);
+    this.physics.add.collider(this.walls, this.otherBullets);
+    // myBullets collide with other players
+    this.physics.add.collider(this.myBullets, this.otherPlayers, function (_bullet, _player) {
+        console.log("testing");
+        alert("ya done hit the dude");
+    });
 }
 
 function update() {
     if (this.ship) {
-
+        if (this.initialX != -1) {
+            this.ship.oldPosition = {x: this.ship.x, y: this.ship.y, rotation: this.ship.rotation};
+            this.ship.x = this.initialX;
+            this.ship.y = this.initialY;
+            this.initialX = -1;
+            this.initialY = -1;
+        }
         // emit new position if it has changed
         var x = this.ship.x;
         var y = this.ship.y;
@@ -85,18 +123,53 @@ function update() {
         }
 
         if (this.cursors.up.isDown) {
-            this.physics.velocityFromRotation(this.ship.rotation + 1.5, -100, this.ship.body.acceleration);
+            this.ship.setVelocityX(Math.sin(this.ship.rotation) * MAX_TANK_SPEED);
+            this.ship.setVelocityY(-Math.cos(this.ship.rotation) * MAX_TANK_SPEED);
+        }
+        else if (this.cursors.down.isDown) {
+            this.ship.setVelocityX(-Math.sin(this.ship.rotation) * MAX_TANK_SPEED);
+            this.ship.setVelocityY(Math.cos(this.ship.rotation) * MAX_TANK_SPEED);
         }
         else {
-            this.ship.setAcceleration(0);
+            this.ship.setVelocityX(0);
+            this.ship.setVelocityY(0);
+        }
+
+        if (this.cursors.space.isDown) {
+            if (this.isShooting == false) {
+                this.isShooting = true;
+                // Shoot a bullet
+                shootBullet(this, this.ship.x, this.ship.y, this.ship.rotation, true);
+                this.socket.emit('shotBullet', {x: this.ship.x, y: this.ship.y, rot: this.ship.rotation});
+            }
+        }
+        else {
+            this.isShooting = false;
         }
 
         this.physics.world.wrap(this.ship, 5);
     }
 }
 
+function shootBullet(self, x, y, angle, mine) {
+    let bullet = self.add.circle(x, y, WIDTH / 400, 0xff00ff);
+    bullet = self.physics.add.existing(bullet);
+    // TODO: Set timeout
+    if (mine) {
+        self.myBullets.add(bullet);
+    }
+    else {
+        self.otherBullets.add(bullet);
+    }
+    bullet.body.bounce.x = 1;
+    bullet.body.bounce.y = 1;
+    bullet.body.velocity.x = Math.sin(angle) * MAX_BULLET_SPEED;
+    bullet.body.velocity.y = -Math.cos(angle) * MAX_BULLET_SPEED;
+}
+
 function addPlayer(self, playerInfo) {
     self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+    self.playerGroup.add(self.ship);
     if (playerInfo.team === 'blue') {
         self.ship.setTint(0x0000ff);
     }
@@ -106,6 +179,8 @@ function addPlayer(self, playerInfo) {
     self.ship.setDrag(100);
     self.ship.setAngularDrag(100);
     self.ship.setMaxVelocity(200);
+    self.ship.body.bounce.x = 1;
+    self.ship.body.bounce.y = 1;
 }
 
 function addOtherPlayers(self, playerInfo) {
@@ -118,4 +193,77 @@ function addOtherPlayers(self, playerInfo) {
     }
     otherPlayer.playerId = playerInfo.playerId;
     self.otherPlayers.add(otherPlayer);
+}
+
+function add_wall(self, x, y, width, height, color) {
+    // Calculate x and y so that they refer to the center of the dumb rectangle
+    x = x + width / 2;
+    y = y + height / 2;
+    color = color * (0.9 + Math.random() * 0.1);
+    let wall = self.add.rectangle(x, y, width, height, color);
+    self.physics.add.existing(wall);
+    wall.body.bounce.x = 1;
+    wall.body.bounce.y = 1;
+    self.walls.add(wall);
+    wall.body.immovable = true;
+}
+
+function draw_map(self, current_map) {
+    let vw = current_map[0];
+    let hw = current_map[1];
+
+    // Calculate total width and height of the map (measured from middle of lines)
+    self.map_width = 0.8 * WIDTH;
+    self.map_height = 0.8 * HEIGHT;
+    self.grid_size = 0;
+
+    if (self.map_width / hw[0].length * vw.length > 0.8 * HEIGHT) {
+        // Limit height instead of width
+        self.grid_size = self.map_height / vw.length;
+        self.map_width = self.grid_size * hw[0].length;
+    }
+    else {
+        // Limit width
+        self.grid_size = self.map_width / hw[0].length;
+        self.map_height = self.grid_size * vw.length;
+    }
+
+    // Calculate line width
+    let line_width = WIDTH / 200;
+
+    // Calculate where to put top left corner
+    let x = WIDTH / 2 - self.map_width / 2;
+    let y = HEIGHT / 2 - self.map_height / 2;
+
+    // Draw outline
+    const map_color = 0x00ff00;
+    // TODO: Add to collision group
+    add_wall(self, x - line_width / 2, y - line_width / 2, self.map_width + line_width, line_width, map_color); // TOP
+    add_wall(self, x - line_width / 2, y + (self.map_height - line_width / 2), self.map_width + line_width, line_width, map_color); // BOTTOM
+    add_wall(self, x - line_width / 2, y + line_width / 2, line_width, self.map_height - line_width, map_color); // LEFT
+    add_wall(self, x + self.map_width - line_width / 2, y + line_width / 2, line_width, self.map_height - line_width, map_color); // RIGHT
+
+    // Add vertical walls
+    for (row = 0; row < vw.length; ++row) {
+        for (col = 0; col < vw[row].length; ++col) {
+            if (vw[row][col] == 1) {
+                // Calculate position of corner
+                let wall_x = x + (col + 1) * self.grid_size - line_width / 2;
+                let wall_y = y + row * self.grid_size - line_width / 2;
+                // Add wall
+                add_wall(self, wall_x, wall_y, line_width, self.grid_size + line_width, map_color);
+            }
+        }
+    }
+
+    // Add horizontal walls
+    for (row = 0; row < hw.length; ++row) {
+        for (col = 0; col < hw[row].length; ++col) {
+            if (hw[row][col]) {
+                let wall_x = x + col * self.grid_size - line_width / 2;
+                let wall_y = y + (row + 1) * self.grid_size - line_width / 2;
+                add_wall(self, wall_x, wall_y, self.grid_size + line_width, line_width, map_color);
+            }
+        }
+    }
 }
--- server.js
+++ server.js
@@ -1,14 +1,191 @@
+const { triggerAsyncId } = require('async_hooks');
+const { SSL_OP_TLS_ROLLBACK_BUG } = require('constants');
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var players = {};
+var current_map = generate_map(8, 6);
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+
+// To represent the map we will have one array for each of the vertical walls and one array for each of the horizontal walls.
+// The number of vertical walls in a row is always one less than the number of grid squares in a row.
+// The number of horizontal walls in a row is always the same as the number of grid squares in a row.
+// The number of vertical walls in a column is always the same as the number of grid squares in a column.
+// The number of horizontal walls in a column is always one less than the number of grid squares in a column.
+// Each grid square has the following walls: vw[i][j-1] vw[i][j] hw[i-1][j] hw[i][j]
+// The output of this function is an array with two elements: the vertical wall array, and the horizontal wall array.
+/*
+function generate_map(width, height) {
+    // Set up the arrays
+    let vw = new Array(height);
+    for (let i = 0; i < height; ++i) {
+        vw[i] = new Array(width - 1).fill(1);
+    }
+    let hw = new Array(height - 1);
+    for (let i = 0; i < height - 1; ++i) {
+        hw[i] = new Array(width).fill(1);
+    }
+
+    // Generate the board
+    for (let i = 0; i < height; ++i) {
+        for (let j = 0; j < width; ++j) {
+            // Choose either horizontal or vertical wall to delete
+            if (Math.floor(Math.random() * 2) == 0) {
+                // Delete a vertical wall. Make sure that we choose one that isn't out of bounds.
+                let d = Math.floor(Math.random() * 2);
+                if (j == width - 1 || (d == 0 && j != 0)) {
+                    // Delete wall to the left
+                    vw[i][j - 1] = 0;
+                }
+                else {
+                    // Delete wall to the right
+                    vw[i][j] = 0;
+                }
+            }
+            else {
+                // Delete a horizontal wall. Make sure that we choose one that isn't out of bounds.
+                let d = Math.floor(Math.random() * 2);
+                if (i == height - 1 || (d == 0 && i != 0)) {
+                    // Delete wall to the left
+                    hw[i - 1][j] = 0;
+                }
+                else {
+                    // Delete wall to the right
+                    hw[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    return [vw, hw];
+}
+*/
+
+// TAKE 2
+function validate_map(map) {
+    let vw = map[0];
+    let hw = map[1];
+    let rows = vw.length;
+    let cols = hw[0].length;
+    // Start at top left grid square
+    // Do a search of some kind
+    // If we don't count all the nodes then it's a bad map
+    let s = [];
+    let explored = new Set();
+    s.push(0);
+    let count = 0;
+
+    while (s.length != 0) {
+        let v = s.pop();
+        explored.add(v);
+        ++count;
+
+        // Add connected vertices if they haven't been visited
+        let r = Math.floor(v / cols);
+        let c = v % cols;
+        // up
+        if (r - 1 >= 0 && hw[r - 1][c] == 0) {
+            let w = (r - 1) * cols + c;
+            if (!explored.has(w)) {
+                s.push(w);
+            }
+        }
+        // down
+        if (r < rows - 1 && hw[r][c] == 0) {
+            let w = (r + 1) * cols + c;
+            if (!explored.has(w)) {
+                s.push(w);
+            }
+        }
+        // left
+        if (c - 1 >= 0 && vw[r][c - 1] == 0) {
+            let w = r * cols + c - 1;
+            if (!explored.has(w)) {
+                s.push(w);
+            }
+        }
+        // right
+        if (c < cols - 1 && vw[r][c] == 0) {
+            let w = r * cols + c + 1;
+            if (!explored.has(w)) {
+                s.push(w);
+            }
+        }
+    }
+
+    return explored.size == rows * cols;
+}
+
+function generate_map(width, height) {
+    while (true) {
+        // Set up the arrays
+        let vw = new Array(height);
+        for (let i = 0; i < height; ++i) {
+            vw[i] = new Array(width - 1).fill(1);
+        }
+        let hw = new Array(height - 1);
+        for (let i = 0; i < height - 1; ++i) {
+            hw[i] = new Array(width).fill(1);
+        }
+
+        // Generate the board
+        for (let i = 0; i < height; ++i) {
+            for (let j = 0; j < width; ++j) {
+                // Choose either horizontal or vertical wall to delete
+                let tries = 0;
+                while (tries >= 0 && tries < 20) {
+                    if (Math.floor(Math.random() * 2) == 0) {
+                        // Delete a vertical wall. Make sure that we choose one that isn't out of bounds.
+                        let d = Math.floor(Math.random() * 2);
+                        if (j == width - 1 || (d == 0 && j != 0)) {
+                            // Delete wall to the left
+                            if (vw[i][j - 1] != 0) {
+                                vw[i][j - 1] = 0;
+                                tries = -2;
+                            }
+                        }
+                        else {
+                            // Delete wall to the right
+                            if (vw[i][j] != 0) {
+                                vw[i][j] = 0;
+                                tries = -2;
+                            }
+                        }
+                    }
+                    else {
+                        // Delete a horizontal wall. Make sure that we choose one that isn't out of bounds.
+                        let d = Math.floor(Math.random() * 2);
+                        if (i == height - 1 || (d == 0 && i != 0)) {
+                            // Delete wall to the left
+                            if (hw[i - 1][j] != 0) {
+                                hw[i - 1][j] = 0;
+                                tries = -2;
+                            }
+                        }
+                        else {
+                            // Delete wall to the right
+                            if (hw[i][j] != 0) {
+                                hw[i][j] = 0;
+                                tries = -2;
+                            }
+                        }
+                    }
+                    ++tries;
+                }
+            }
+        }
+        if (validate_map([vw, hw])) {
+            return [vw, hw];
+        }
+    }
+}
+// END OF TAKE 2
 
 io.on('connection', function(socket) {
     console.log('a user connected!');
@@ -24,6 +201,9 @@ io.on('connection', function(socket) {
 
     // Send the players object to the new player
     socket.emit('currentPlayers', players);
+    // Send the map to the new player
+    socket.emit('current_map', current_map);
+
     // update all other players of the new player
     socket.broadcast.emit('newPlayer', players[socket.id]);
 
@@ -44,6 +224,11 @@ io.on('connection', function(socket) {
 
         // broadcast new data to all players
         socket.broadcast.emit('playerMoved', players[socket.id]);
+    });
+
+    // When a player shoots a bullet, shoot the bullet.
+    socket.on('shotBullet', function (bulletData) {
+        socket.broadcast.emit('shotBullet', bulletData);
     });
 });
 
